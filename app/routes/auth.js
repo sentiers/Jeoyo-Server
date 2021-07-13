acb368526e86531c48c737bc46e57b7e693c838f
Hyunji Cho <sentiersg@gmail.com>
2021-07-13 09:33:45 +0900

    finished /auth/register

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,5 +1,7 @@
 //====HANDLE AUTHORIZATION ROUTES ============================
 var router = require('express').Router();
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
 var UserData = require('../models/userData');
 var User = require('../models/user');
 
@@ -12,37 +14,35 @@ var User = require('../models/user');
 //==== 유저 등록 =========================
 function registerUser(data) {
     return new Promise(function (resolve, reject) {
-        var newUser = new User();
-        newUser.user_email = data.user_email;
-        bcrypt.genSalt(10, function (err, salt) {
-            bcrypt.hash(data.user_password, salt, function (err, hash) {
+        var newUser = new User(data);
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.user_password, salt, (err, hash) => {
                 if (err) {
-                    reject("에러: " + err);
-                } else {
-                    newUser.user_password = hash;
-                    newUser.save((err) => {
-                        if (err) {
-                            if (err.code == 11000) {
-                                reject("에러: 해당 이메일은 사용중입니다");
-                            } else {
-                                reject("에러: " + err);
-                            }
-                        } else {
-                            var newUserData = new UserData();
-                            newUserData.user_name = data.user_name;
-                            newUserData.user_email = data.user_email;
-                            newUserData.save((err) => {
-                                if (err) {
-                                    reject("에러: " + err);
-                                } else {
-                                    resolve("성공: 유저 등록");
-                                }
-                            });
-                        }
-                    });
+                    reject(err);
                 }
-            });
-        });
+                newUser.user_password = hash;
+                newUser.save((err) => {
+                    if (err) {
+                        if (err.code == 11000) {
+                            reject("에러: 해당 이메일은 사용중입니다");
+                        } else {
+                            reject("에러: " + err);
+                        }
+                    } else {
+                        var newUserData = new UserData();
+                        newUserData.user_name = data.user_name;
+                        newUserData.user_email = data.user_email;
+                        newUserData.save((err) => {
+                            if (err) {
+                                reject("에러: " + err);
+                            } else {
+                                resolve("성공: 유저 등록");
+                            }
+                        });
+                    }
+                });
+            })
+        })
     });
 };
 
@@ -65,7 +65,6 @@ router.post('/register', function (req, res, next) {
     registerUser(req.body)
         .then((msg) => {
             res.send(msg);
-            console.log(msg);
         }).catch((err) => {
             res.send(err);
         });
@@ -83,7 +82,10 @@ router.post('/find', function (req, res, next) {
 
 //====로그인 =============================
 router.post('/login', function (req, res, next) {
-    passport.authenticate('local')(req, res, next);
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/auth',
+    })(req, res, next);
 });
 
 //====비밀번호 변경 =============================
@@ -93,7 +95,7 @@ router.post('/update', function (req, res, next) {
 
 //====로그아웃 =============================
 router.get('/logout', function (req, res, next) {
-    req.logout();
+    req.logout().then(console.log("로그아웃"));
 });
 
 //====google 계정으로 로그인 =================

10ccf29b7b98a066d5cf6dcd46b476bd721dc3f2
Hyunji Cho <sentiersg@gmail.com>
2021-07-16 04:52:58 +0900

    ensure authenticated function

--- app/config/ensureAuth.js
+++ app/config/ensureAuth.js
@@ -1,2 +1,10 @@
-// ensure Auth 
-// https://github.com/bradtraversy/node_passport_login/blob/master/config/auth.js
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/401');
+    },
+  };
+
+  //https://github.com/bradtraversy/node_passport_login/blob/master/config/auth.js

34f59fa504a3c75b484c538f35e2f95c379ce672
Hyunji Cho <sentiersg@gmail.com>
2021-07-19 16:19:15 +0900

    changed for testing

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -155,9 +155,9 @@ router.get('/', function (req, res, next) {
 router.post('/register', function (req, res, next) {
     registerUser(req.body)
         .then((msg) => {
-            res.status(200).send(msg);
+            res.status(200).send();
         }).catch((err) => {
-            res.status(401).send(err);
+            res.status(401).send();
         });
 });
 
@@ -177,9 +177,9 @@ router.post('/login',
     function (req, res, next) {
         isFirstLogin(req.session.passport.user)
             .then((msg) => {
-                res.status(200).send(msg);
+                res.status(200).send();
             }).catch((err) => {
-                res.status(401).send(err);
+                res.status(401).send();
             });
     });
 

ac07d7045de1d876725b94f549b420e28016cc7b
Hyunji Cho <sentiersg@gmail.com>
2021-07-24 12:16:39 +0900

    right before starting /post part

--- app/routes/menu-explore-post.js
+++ app/routes/menu-explore-post.js
@@ -1,40 +1,6 @@
 //====HANDLE POST ROUTES =============
 var router = require('express').Router();
 
-//====  ============================
-function registerUser(data) {
-    return new Promise(function (resolve, reject) {
-
-        
-    });
-};
-
-//====테스팅 용도 =============================
-router.get('/', function (req, res, next) {
-    res.send("hello post");
-});
-
-
-//====  ============================
-router.post('/register', function (req, res, next) {
-    registerUser(req.body)
-        .then((code) => {
-            res.status(code).send(code + ": 회원가입 성공");
-        }).catch((errcode) => {
-            res.status(errcode).send(errcode + ": 회원가입 실패");
-        });
-});
-
-//====  =============================
-router.post('/termsofuse', function (req, res, next) {
-    termsOfUse(req.session.passport.user, req.body)
-        .then((code) => {
-            res.status(code).send(code + ": 이용약관 동의 완료");
-        }).catch((errcode) => {
-            res.status(errcode).send(errcode + ": 이용약관 동의시 문제가 발생하였습니다");
-        });
-});
-
 
 
 

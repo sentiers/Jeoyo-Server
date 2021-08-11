fde0a19d82fc777002de9cfa038aad6e6ae76e1a
Hyunji Cho <sentiersg@gmail.com>
2021-08-11 11:43:58 +0900

    finished likedUsers features

--- app/routes/menu-explore-user.js
+++ app/routes/menu-explore-user.js
@@ -5,6 +5,7 @@ var Post = require('../models/post');
 var moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
+var ObjectId = require('mongodb').ObjectID;
 // ----------------------------------------------------------------
 
 //==== 유저 id 별로 조회 =========================
@@ -17,6 +18,58 @@ function getUserById(idData) {
         }).catch((err) => {
             reject(404);
         });
+    });
+};
+
+//==== 하트가 차있는지 아닌지 확인 ================
+function isLiked(email, idData) {
+    return new Promise(function (resolve, reject) {
+        UserData.findOne({
+            $and: [
+                { user_email: email },
+                { user_likedUsers: { $elemMatch: { $eq: ObjectId(idData) } } }
+            ]
+        }).then((user) => {
+            if (user) {
+                resolve(1);
+            } else {
+                resolve(0);
+            }
+        })
+    });
+};
+
+//==== 관심유저 추가/삭제 함수 =========================
+function likeUser(email, idData, isLiked) {
+    return new Promise(function (resolve, reject) {
+        if (isLiked == 1) {
+            UserData.updateOne(
+                { user_email: email },
+                {
+                    $pull: { 'user_likedUsers': ObjectId(idData) }
+                }
+            ).then(() => {
+                resolve(200);
+            }).catch((err) => {
+                reject(500);
+            });
+        } else {
+            UserData.updateOne(
+                { user_email: email },
+                {
+                    $push: {
+                        'user_likedUsers': {
+                            $each: [ObjectId(idData)],
+                            $position: 0
+                        }
+                    }
+                }
+            ).then(() => {
+                resolve(200);
+            }).catch((err) => {
+                reject(500);
+            });
+        }
     });
 };
 
@@ -84,20 +137,26 @@ function getAllUsers() {
 router.get('/:id', function (req, res, next) {
     getUserById(req.params.id)
         .then((data) => {
-            res.status(data[0]).send(data[1]);
+            isLiked(req.user.user_email, req.params.id)
+                .then((liked) => {
+                    res.status(data[0]).send({ user: data[1], isLiked: liked });
+                })
         }).catch((errcode) => {
             res.status(errcode).send(errcode + ": 유저 가져오기 실패");
         });
 });
 
-//==== GET 해당 id 유저 관심 유저로 추가 =============================
+//==== GET 해당 id 유저 관심 유저로 추가/삭제 =============================
 router.get('/like/:id', function (req, res, next) {
-    likeUser(req.user.user_email, req.params.id)
-        .then((data) => {
-            res.status(data[0]).send(data[1]);
-        }).catch((errcode) => {
-            res.status(errcode).send(errcode + ": 관심유저추가 실패");
-        });
+    isLiked(req.user.user_email, req.params.id)
+        .then((liked) => {
+            likeUser(req.user.user_email, req.params.id, liked)
+                .then((code) => {
+                    res.status(code).send(code + ": 관심유저 추가/삭제 성공");
+                }).catch((errcode) => {
+                    res.status(errcode).send(errcode + ": 관심유저 추가/삭제 실패");
+                });
+        })
 });
 
 //==== GET 유저 필터링 =============================

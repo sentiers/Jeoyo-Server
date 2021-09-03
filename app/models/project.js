bfb132f5c097890f611cadc4f85b1efb7d252b95
Hyunji Cho <sentiersg@gmail.com>
2021-09-03 23:01:05 +0900

    project schema

--- app/models/project.js
+++ app/models/project.js
@@ -5,6 +5,7 @@ var ObjectId = require('mongodb').ObjectID;
 // 프로젝트 스키마
 var ProjectSchema = new Schema({
     project_title: String, // 프로젝트 제목
+    project_img: String, // 프로젝트 이미지
     project_leader: { // 팀장 정보
         id: ObjectId,
         email: String,
